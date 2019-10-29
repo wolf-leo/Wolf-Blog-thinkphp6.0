a18401a3eed96dbb2a4412c2ffea5997c40c1a82
wolf-leo <37436228+wolf-leo@users.noreply.github.com>
2019-10-30 01:21:51 +0800

    re

--- app/common/define.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-define('WEB_TITLE', "My Blog");
-define('WEB_ICP', "闽ICP备XXXX号-X");
-
-
--- runtime/services.php
+++
@@ -1,5 +0,0 @@
-<?php 
-// This cache file is automatically generated at:2019-06-03 17:34:35
-declare (strict_types = 1);
-return array (
-);
