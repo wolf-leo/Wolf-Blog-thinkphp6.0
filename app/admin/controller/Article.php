a182315b36821fa277e74a8fe89c4a9b5e41a09e
wolfcode <279387808@qq.com>
2020-07-02 16:57:55 +0800

    修复文章上传图片失败

--- app/admin/controller/Article.php
+++ app/admin/controller/Article.php
@@ -5,93 +5,94 @@ namespace app\admin\controller;
 use app\AdminBaseController;
 use think\facade\View;
 
-class Article extends AdminBaseController {
+class Article extends AdminBaseController
+{
 
-	protected $mod;
+    protected $mod;
 
-	public function __construct() {
-		parent::__construct();
-		$this->mod = new \app\admin\model\Article();
-		$category = new \app\admin\model\Category();
-		$type = $category->where(['status' => 1])->orderRaw('sort')->column('title', 'id');
-		View::assign([
-			'notes' => $this->mod->notes,
-			'type' => $type,
-		]);
-	}
+    public function __construct() {
+        parent::__construct();
+        $this->mod = new \app\admin\model\Article();
+        $category = new \app\admin\model\Category();
+        $type = $category->where(['status' => 1])->orderRaw('sort')->column('title', 'id');
+        View::assign([
+            'notes' => $this->mod->notes,
+            'type' => $type,
+        ]);
+    }
 
-	public function index() {
-		$pageSize = 5; //每页显示的数量
-		$where = [];
-		if (input('id')) {
-			$where[] = ['id', '=', input('id')];
-		}
-		$list = $this->mod->where($where)->order('id', 'desc')->paginate($pageSize);
-		View::assign(['list' => $list]);
-		return $this->adminTpl();
-	}
+    public function index() {
+        $pageSize = 5; //每页显示的数量
+        $where = [];
+        if (input('id')) {
+            $where[] = ['id', '=', input('id')];
+        }
+        $list = $this->mod->where($where)->order('id', 'desc')->paginate($pageSize);
+        View::assign(['list' => $list]);
+        return $this->adminTpl();
+    }
 
-	/**
-	 * Windows 环境下如果遇到
-	 * upload_tmp_dir 临时文件夹问题
-	 * 上传文件提示
-	 * Warning: File upload error - unable to create a temporary file in Unknown on line 0
-	 * 找到php.ini 中的 upload_tmp_dir 把前边的“；”去掉然后改为upload_tmp_dir =C:\Windows\temp
-	 * 最后记得重启apache
-	 */
-	public function edit() {
-		$id = input('id/d', 0);
-		$info = $this->mod->where('id', $id)->find();
-		if (!request()->isPost()) {
-			View::assign(['info' => $info,]);
-			return $this->adminTpl();
-		}
-		$data = input('post.');
-		unset($data['id']);
-		$file = '';
-		$file_path = base_path() . 'public' . DIRECTORY_SEPARATOR . 'uploads';
-		if (!empty($_FILES['img']['tmp_name'])) {
-			$file = request()->file('img'); //图片上传
-		}
-		if ($file) {
-			$img_path = config()['filesystem']['disks']['public']['url'];
-			$savename = \think\facade\Filesystem::disk('public')->putFile('topic', $file, 'md5');
-			if ($savename) {
-				$data['img'] = $img_path . DIRECTORY_SEPARATOR . $savename;
-			} else {
-				$this->jump(0, $file->getError());
-			}
-		}
-		if ($id) { //更新数据
-			$where['id'] = $id;
-			$x = $this->mod->where($where)->update($data);
-		} else { //添加数据
-			$data['c_time'] = date('Y-m-d H:i:s');
-			$x = $this->mod->insertGetId($data);
-		}
-		$x and $this->jump(1, '修改成功') or $this->jump(0, '修改失败');
-	}
+    /**
+     * Windows 环境下如果遇到
+     * upload_tmp_dir 临时文件夹问题
+     * 上传文件提示
+     * Warning: File upload error - unable to create a temporary file in Unknown on line 0
+     * 找到php.ini 中的 upload_tmp_dir 把前边的“；”去掉然后改为upload_tmp_dir =C:\Windows\temp
+     * 最后记得重启apache
+     */
+    public function edit() {
+        $id = input('id/d', 0);
+        $info = $this->mod->where('id', $id)->find();
+        if (!request()->isPost()) {
+            View::assign(['info' => $info,]);
+            return $this->adminTpl();
+        }
+        $data = input('post.');
+        unset($data['id']);
+        $file = '';
+        $file_path = base_path() . 'public' . DIRECTORY_SEPARATOR . 'uploads';
+        if (!empty($_FILES['img']['tmp_name'])) {
+            $file = request()->file('img'); //图片上传
+        }
+        if ($file) {
+            $img_path = config()['filesystem']['disks']['public']['url'];
+            $savename = \think\facade\Filesystem::disk('public')->putFile('topic', $file, 'md5');
+            if ($savename) {
+                $data['img'] = $img_path . DIRECTORY_SEPARATOR . $savename;
+            } else {
+                $this->jump(0, $file->getError());
+            }
+        }
+        if ($id) { //更新数据
+            $where['id'] = $id;
+            $x = $this->mod->where($where)->update($data);
+        } else { //添加数据
+            $data['c_time'] = date('Y-m-d H:i:s');
+            $x = $this->mod->insertGetId($data);
+        }
+        $x and $this->jump(1, '修改成功') or $this->jump(0, '修改失败');
+    }
 
-	/**
-	 * Windows 环境下如果遇到
-	 * upload_tmp_dir 临时文件夹问题
-	 * 上传文件提示
-	 * Warning: File upload error - unable to create a temporary file in Unknown on line 0
-	 * 找到php.ini 中的 upload_tmp_dir 把前边的“；”去掉然后改为upload_tmp_dir =C:\Windows\temp
-	 * 最后记得重启apache
-	 */
-	//  编辑器图片上传 【单张上传操作，多图上传自行研究- -】
-	public function UploadPic() {
-		$file = request()->file('info_upload_img');
-		if ($file) {
-			if ($img_info) {
-				$img = $img_path . $img_info->getSaveName();
-				$ret = ["errno" => 0, 'data' => [$img]];
-				return json($ret);
-			} else {
-				$this->jump(0, $file->getError());
-			}
-		}
-	}
+    /**
+     * Windows 环境下如果遇到
+     * upload_tmp_dir 临时文件夹问题
+     * 上传文件提示
+     * Warning: File upload error - unable to create a temporary file in Unknown on line 0
+     * 找到php.ini 中的 upload_tmp_dir 把前边的“；”去掉然后改为upload_tmp_dir =C:\Windows\temp
+     * 最后记得重启apache
+     */
+    //  编辑器图片上传 【单张上传操作，多图上传自行研究- -】
+    public function UploadPic() {
+        $file = request()->file('info_upload_img');
+        // 上传到本地服务器
+        $savename = \think\facade\Filesystem::disk('public')->putFile('articlepic', $file);
+        if ($savename) {
+            $img = "/uploads/" . $savename;
+            $ret = ["errno" => 0, 'data' => [$img]];
+            return json($ret);
+        } else {
+            $this->jump(0, $file->getError());
+        }
+    }
 
 }
--- app/admin/view/article/edit.php
+++ app/admin/view/article/edit.php
@@ -1,98 +1,98 @@
 <link rel="stylesheet" href="/src/admin/wangEditor/css/wangEditor.min.css">
 <script type="text/javascript" src="/src/admin/wangEditor/js/wangEditor.min.js"></script>
 <div class="aright">
-	<fieldset class="layui-elem-field layui-field-title" style="margin: 20px 30px 20px 20px;">
-		<legend><?php echo isset($info['id']) ? '修改' : '添加'; ?>文章</legend>
-	</fieldset>
-	<form class="layui-form bform" method="post" enctype="multipart/form-data">
-		<div class="layui-form-item">
-			<label class="layui-form-label">文章标题</label>
-			<div class="layui-input-block">
-				<input type="text" name="title" required lay-verify="required" value="{$info.title}" placeholder="必填内容" autocomplete="off" class="layui-input">
-			</div>
-		</div>
-		<div class="layui-form-item">
-			<label class="layui-form-label">缩略图</label>
-			<div class="layui-input-block">
-				<div class="file-box">
-					<i class="layui-icon">&#xe61f;</i>
-					<input class="file-btn" type="button" value="选择图片"> 
-					<input class="file-txt" type="text" autocomplete="off"  id="textfield">
-					{if ($info['img'])}<img src="{$info.img|default=''}" width="120">{else /}{/if}
-					<input class="file-file" type="file" name="img" id="pic" size="28" onchange="document.getElementById('textfield').value = this.value" /> 
-				</div>
-			</div>
-		</div>
-		<div class="layui-form-item" style="width: 300px;">
-			<label class="layui-form-label">文章类型</label>
-			<div class="layui-input-block">
-				<select name="type">
-					<option value="0">请选择</option>
-					{volist name='type' id='vo'}
-					<option {if ($info['type']==$key)}selected="selected"{/if} value="{$key}">{$vo}</option>
-					{/volist}
-				</select>
-			</div>
-		</div>
-		<div class="layui-form-item" style="width: 300px;">
-			<label class="layui-form-label">文章状态</label>
-			<div class="layui-input-block">
-				<select name="status">
-					<option value="0">请选择</option>
-					{volist name='notes.status' id='vo'}
-					<option {if ($info['status']==$key)}selected="selected"{/if} value="{$key}">{$vo}</option>
-					{/volist}
-				</select>
-			</div>
-		</div>
-		<div class="layui-form-item" style="width: 300px;">
-			<label class="layui-form-label">推荐到Banner</label>
-			<div class="layui-input-block">
-				<select name="isbanner">
-					<option value="0">请选择</option>
-					{volist name='notes.isbanner' id='vo'}
-					<option {if ($info['isbanner']==$key)}selected="selected"{/elseif (!$info['isbanner'] && $key==2)}selected="selected"{/if} value="{$key}">{$vo}</option>
-					{/volist}
-				</select>
-			</div>
-		</div>
-		<div class="layui-form-item">
-			<label class="layui-form-label">排序</label>
-			<div class="layui-input-block">
-				<input type="text" name="sort" required lay-verify="required" value="{$info.sort}" placeholder="越小靠前" autocomplete="off" class="layui-input">
-			</div>
-		</div>
-		<div class="layui-form-item">
-			<label class="layui-form-label">文章内容</label>
-			<div class="layui-input-block">
-				<div id="textarea" style='width:70%;height:550px;'>
-					<p>{$info.content|raw}</p>
-				</div>
-				<textarea id="text1" style="display: none;" name="content"></textarea>
-			</div>
-		</div>
-		<div class="layui-form-item">
-			<div class="layui-input-block">
-				<input type="hidden" name="id" value="{$info.id}">
-				<button class="layui-btn" lay-filter="formDemo" >立即提交</button> 
-			</div>
-		</div>
-	</form>
+    <fieldset class="layui-elem-field layui-field-title" style="margin: 20px 30px 20px 20px;">
+        <legend><?php echo isset($info['id']) ? '修改' : '添加'; ?>文章</legend>
+    </fieldset>
+    <form class="layui-form bform" method="post" enctype="multipart/form-data">
+        <div class="layui-form-item">
+            <label class="layui-form-label">文章标题</label>
+            <div class="layui-input-block">
+                <input type="text" name="title" required lay-verify="required" value="{$info.title}" placeholder="必填内容" autocomplete="off" class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">缩略图</label>
+            <div class="layui-input-block">
+                <div class="file-box">
+                    <i class="layui-icon">&#xe61f;</i>
+                    <input class="file-btn" type="button" value="选择图片"> 
+                    <input class="file-txt" type="text" autocomplete="off"  id="textfield">
+                    {if ($info['img'])}<img src="{$info.img|default=''}" width="120">{else /}{/if}
+                    <input class="file-file" type="file" name="img" id="pic" size="28" onchange="document.getElementById('textfield').value = this.value" /> 
+                </div>
+            </div>
+        </div>
+        <div class="layui-form-item" style="width: 300px;">
+            <label class="layui-form-label">文章类型</label>
+            <div class="layui-input-block">
+                <select name="type">
+                    <option value="0">请选择</option>
+                    {volist name='type' id='vo'}
+                    <option {if ($info['type']==$key)}selected="selected"{/if} value="{$key}">{$vo}</option>
+                    {/volist}
+                </select>
+            </div>
+        </div>
+        <div class="layui-form-item" style="width: 300px;">
+            <label class="layui-form-label">文章状态</label>
+            <div class="layui-input-block">
+                <select name="status">
+                    <option value="0">请选择</option>
+                    {volist name='notes.status' id='vo'}
+                    <option {if ($info['status']==$key)}selected="selected"{/if} value="{$key}">{$vo}</option>
+                    {/volist}
+                </select>
+            </div>
+        </div>
+        <div class="layui-form-item" style="width: 300px;">
+            <label class="layui-form-label">推荐到Banner</label>
+            <div class="layui-input-block">
+                <select name="isbanner">
+                    <option value="0">请选择</option>
+                    {volist name='notes.isbanner' id='vo'}
+                    <option {if ($info['isbanner']==$key)}selected="selected"{/elseif (!$info['isbanner'] && $key==2)}selected="selected"{/if} value="{$key}">{$vo}</option>
+                    {/volist}
+                </select>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">排序</label>
+            <div class="layui-input-block">
+                <input type="text" name="sort" required lay-verify="required" value="{$info.sort}" placeholder="越小靠前" autocomplete="off" class="layui-input">
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <label class="layui-form-label">文章内容</label>
+            <div class="layui-input-block">
+                <div id="textarea" style='width:70%;height:550px;'>
+                    <p>{$info.content|raw}</p>
+                </div>
+                <textarea id="text1" style="display: none;" name="content"></textarea>
+            </div>
+        </div>
+        <div class="layui-form-item">
+            <div class="layui-input-block">
+                <input type="hidden" name="id" value="{$info.id}">
+                <button class="layui-btn" lay-filter="formDemo" >立即提交</button> 
+            </div>
+        </div>
+    </form>
 </div>
 
 <script>
-		layui.use('form', function () {
-			var form = layui.form();
-		});
-		var E = window.wangEditor;
-		var editor = new E('#textarea');
-		var $text1 = $('#text1');
-		editor.customConfig.uploadImgServer = "{:url('Article/UploadPic')}";
-		editor.customConfig.uploadFileName = 'info_upload_img';
-		editor.customConfig.onchange = function (html) {
-			$text1.val(html);
-		};
-		editor.create();
-		$text1.val(editor.txt.html());
-		$('#textarea .w-e-text,.w-e-text-container').attr('style', 'border: 1px solid #ccc;width:100%;height:500px;');
+    layui.use('form', function () {
+        var form = layui.form();
+    });
+    var E = window.wangEditor;
+    var editor = new E('#textarea');
+    var $text1 = $('#text1');
+    editor.customConfig.uploadImgServer = "{:url('/Article/UploadPic')}";
+    editor.customConfig.uploadFileName = 'info_upload_img';
+    editor.customConfig.onchange = function (html) {
+        $text1.val(html);
+    };
+    editor.create();
+    $text1.val(editor.txt.html());
+    $('#textarea .w-e-text,.w-e-text-container').attr('style', 'border: 1px solid #ccc;width:100%;height:500px;');
 </script>
